acb3b0582525f78f72046c4f374c2d76d370c846
emilysoehler <emilysoehler@web.de>
2018-07-04 11:25:27 +0200

    corrected the calculation of balanced accuracy

--- metrics.py
+++ metrics.py
@@ -1,7 +1,7 @@
-from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import recall_score
 
 def balanced_accuracy(y_true, y_pred):
-    precision = precision_score(y_true, y_pred)
-    recall = recall_score(y_true, y_pred)
+    specificity = recall_score(y_true, y_pred, pos_label=0)
+    sensitivity = recall_score(y_true, y_pred, pos_label=1)
 
-    return (precision + recall) / 2
+    return (specificity + sensitivity) / 2
