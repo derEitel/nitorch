d25f82873b5ab55225b214b125bd66b5db9616ad
emilysoehler <emilysoehler@web.de>
2018-07-05 14:18:24 +0200

    fixed order

--- metrics.py
+++ metrics.py
@@ -1,15 +1,15 @@
 from sklearn.metrics import recall_score
 
 
-def balanced_accuracy(y_true, y_pred):
-    specificity = specificity(y_true, y_pred)
-    sensitivity = sensitivity(y_true, y_pred)
-    return (specificity + sensitivity) / 2
-
-
 def specificity(y_true, y_pred):
     return recall_score(y_true, y_pred, pos_label=0)
 
 
 def sensitivity(y_true, y_pred):
     return recall_score(y_true, y_pred, pos_label=1)
+
+
+def balanced_accuracy(y_true, y_pred):
+    specificity = specificity(y_true, y_pred)
+    sensitivity = sensitivity(y_true, y_pred)
+    return (specificity + sensitivity) / 2
