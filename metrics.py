9bc369560813079a819a21f28a76fc2b95a86c45
emilysoehler <emilysoehler@web.de>
2018-07-05 11:13:49 +0200

    recall and specificity definitions

--- metrics.py
+++ metrics.py
@@ -1,7 +1,15 @@
 from sklearn.metrics import recall_score
 
+
 def balanced_accuracy(y_true, y_pred):
     specificity = recall_score(y_true, y_pred, pos_label=0)
-    sensitivity = recall_score(y_true, y_pred, pos_label=1)
+    recall = recall_score(y_true, y_pred, pos_label=1)
+    return (specificity + recall) / 2
 
-    return (specificity + sensitivity) / 2
+
+def specificity(y_true, y_pred):
+	return recall_score(y_true, y_pred, pos_label=0)
+
+
+def recall(y_true, y_pred):
+	return recall_score(y_true, y_pred, pos_label=1)
