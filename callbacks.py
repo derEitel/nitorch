410091534ab678a0770c51f73c54eb19b680ff53
derEitel <fabieitel@gmx.de>
2018-07-31 14:46:50 +0200

    introduced early stopping - this will break existing code!

--- callbacks.py
+++ callbacks.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 
 
 class ModelCheckpoint:
-    def __init__(self, path, retain_metric, mode, ignore_before):
+    def __init__(self, path, retain_metric, mode, ignore_before=0):
         self.path = path
         self.retain_metric = retain_metric
         self.mode = mode
@@ -10,12 +10,15 @@ class ModelCheckpoint:
         self.best_res = -1
         self.best_model = None
 
-    def __call__(self, epoch, model, val_metrics):
+    def __call__(self, trainer, epoch, val_metrics):
         if epoch >= self.ignore_before:
-            current_res = val_metrics[self.retain_metric.__name__][-1]
+            if isinstance(self.retain_metric, str):
+                current_res = val_metrics[self.retain_metric][-1]
+            else:
+                current_res = val_metrics[self.retain_metric.__name__][-1]
             if self.compare(current_res):
                 self.best_res = current_res
-                self.best_model = deepcopy(model.state_dict())
+                self.best_model = deepcopy(trainer.model.state_dict())
 
     def compare(self, res):
         if self.mode == "max":
@@ -36,9 +39,13 @@ class ModelCheckpoint:
 
     def final(self):
         """ get best model and reset parameters. """
+        if isinstance(self.retain_metric, str):
+            name = self.retain_metric
+        else:
+            name = self.retain_metric.__name__
         print(
             "Best validation {} at {} after training.".format(
-                self.retain_metric.__name__, self.best_res
+                name, self.best_res
             )
         )
         if self.best_model is not None:
@@ -50,3 +57,60 @@ class ModelCheckpoint:
             print("Minimum iterations to store model not reached.")
             self.reset()
             return self.best_res, best_model
+
+
+class EarlyStopping:
+    """ Stop training when a monitored quantity has stopped improving.
+
+    # Arguments
+        patience: number of iterations without improvement after which to stop
+        retain_metric: the metric which you want to monitor
+        mode: {min or max}; defines if you want to maximise or minimise 
+            your metric
+        ignore_before: does not start the first window until this epoch.
+            Can be useful when training spikes a lot in early epochs.
+    """
+
+
+    def __init__(self, patience, retain_metric, mode, ignore_before=0):
+        self.patience = patience
+        self.retain_metric = retain_metric
+        self.mode = mode
+        self.ignore_before = ignore_before
+        self.best_res = -1
+        # set to first iteration which is interesting
+        self.best_epoch = self.ignore_before
+
+    def __call__(self, trainer, epoch, val_metrics):
+        if epoch >= self.ignore_before:
+            if epoch - self.best_epoch < self.patience:
+                if isinstance(self.retain_metric, str):
+                    current_res = val_metrics[self.retain_metric][-1]
+                else:
+                    current_res = val_metrics[self.retain_metric.__name__][-1]
+                if self.compare(current_res):
+                    self.best_res = current_res
+                    self.best_epoch = epoch
+            else:
+                # end training run
+                trainer.stop_training = True
+
+    def compare(self, res):
+        if self.mode == "max":
+            return res > self.best_res
+        elif self.mode == "min":
+            # check if still standard value
+            if self.best_res == -1:
+                return True
+            else:
+                return res < self.best_res
+        else:
+            raise NotImplementedError("Only modes 'min' and 'max' available")
+
+    def reset(self):
+        """ Resets after training. Useful for cross validation. """
+        self.best_res = -1
+        self.best_epoch = self.ignore_before
+
+    def final(self):
+        self.reset()
--- trainer.py
+++ trainer.py
@@ -3,217 +3,252 @@ import numpy as np
 import torch
 from torch.autograd import Variable
 import torch.nn.functional as F
+from torch.nn import Module
 from sklearn.metrics import confusion_matrix
 import itertools
 import matplotlib.pyplot as plt
 from nitorch.callbacks import ModelCheckpoint
 
 
-def train_model(
-    net,
-    criterion,
-    optimizer,
-    scheduler,
-    train_loader,
-    val_loader,
-    train_sampler,
-    val_sampler,
-    gpu=0,
-    num_epochs=25,
-    show_train_steps=25,
-    show_validation_epochs=1,
-    metrics=[],
-    callbacks=[],
-    class_threshold=0.5,
-):
-    """ Main function to train a network."""
+class Trainer:
+    def __init__(
+        self, 
+        model, 
+        criterion,
+        optimizer,
+        scheduler,
+        metrics=[], 
+        callbacks=[],
+        class_threshold=0.5
+        ):
+        if not isinstance(model, Module):
+            raise ValueError('Expects model type to be torch.nn.Module')
+        self.model = model
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.metrics = metrics
+        self.callbacks = callbacks
+        self.class_threshold = class_threshold
+        self.stop_training = False
+        self.start_time = None
 
-    total_loss = []
-    val_total_loss = []
+    def train_model(
+        self,
+        train_loader,
+        val_loader,
+        train_sampler,
+        val_sampler,
+        gpu=0,
+        num_epochs=25,
+        show_train_steps=25,
+        show_validation_epochs=1,
+        ):
+        """ Main function to train a network."""
 
-    val_metrics = dict()
-    train_metrics = dict()
+        val_metrics = dict()
+        train_metrics = dict()
 
-    start_time = time.time()
-    best_acc = 0.0
-    for epoch in range(num_epochs):
-        running_loss = 0.0
-        epoch_loss = 0.0
-        scheduler.step(epoch)
-        # variables to compute metrics
-        all_preds = []
-        all_labels = []
-        # train
-        net.train()
-        for i, data in enumerate(train_loader):
-            inputs, labels = data["image"], data["label"]
-            # wrap data in Variable
-            inputs, labels = Variable(inputs.cuda(gpu)), Variable(labels.cuda(gpu))
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)  # BCEWithlogits computs sigmoid itself
-            loss.backward()
-            optimizer.step()
-            # print_gradients(net)
-
-            # store results
-            sigmoid = F.sigmoid(outputs)
-            predicted = sigmoid.data >= class_threshold
-            for j in range(len(predicted)):
-                all_preds.append(predicted[j].cpu().numpy()[0])
-                all_labels.append(labels[j].cpu().numpy()[0])
-
-            # print statistics
-            running_loss += loss.item()
-            epoch_loss += loss.item()
-            if i % show_train_steps == 0:  # print every X mini-batches
-                print(
-                    "[%d, %5d] loss: %.3f"
-                    % (epoch + 1, i + 1, running_loss / show_train_steps)
-                )
+        self.start_time = time.time()
+        best_acc = 0.0
+        
+        for epoch in range(num_epochs):
+            if self.stop_training:
+                # TODO: check position of this
+                print("Early stopping in epoch {}".format(epoch - 1))
+                return self.finish_training([train_metrics, val_metrics])
+            else:
                 running_loss = 0.0
-
-        # report training metrics after each epoch
-        train_metrics = report_metrics(
-            train_metrics, metrics, all_labels, all_preds, phase="Train"
-        )
-
-        epoch_loss /= len(train_loader)
-        total_loss.append(epoch_loss)
-
-        # validate every x iterations
-        if epoch % show_validation_epochs == 0:
-            net.eval()
-            validation_loss = 0.0
-            all_preds = []
-            all_labels = []
-
-            with torch.no_grad():
-                for i, data in enumerate(val_loader):
+                epoch_loss = 0.0
+                self.scheduler.step(epoch)
+                # variables to compute metrics
+                all_preds = []
+                all_labels = []
+                # train
+                self.model.train()
+                for i, data in enumerate(train_loader):
                     inputs, labels = data["image"], data["label"]
                     # wrap data in Variable
-                    inputs, labels = (
-                        Variable(inputs.cuda(gpu)),
-                        Variable(labels.cuda(gpu)),
-                    )
-                    # forward pass only
-                    outputs = net(inputs)
-                    # compute validation accuracy
+                    inputs, labels = Variable(inputs.cuda(gpu)), Variable(labels.cuda(gpu))
+                    # zero the parameter gradients
+                    self.optimizer.zero_grad()
+
+                    # forward + backward + optimize
+                    outputs = self.model(inputs)
+                    loss = self.criterion(outputs, labels)  # BCEWithlogits computs sigmoid itself
+                    loss.backward()
+                    self.optimizer.step()
+
+                    # store results
                     sigmoid = F.sigmoid(outputs)
-                    predicted = sigmoid.data >= class_threshold
-                    loss = criterion(outputs, labels)
+                    predicted = sigmoid.data >= self.class_threshold
                     for j in range(len(predicted)):
                         all_preds.append(predicted[j].cpu().numpy()[0])
                         all_labels.append(labels[j].cpu().numpy()[0])
 
-                    validation_loss += loss.item()
+                    # print statistics
+                    running_loss += loss.item()
+                    epoch_loss += loss.item()
+                    if i % show_train_steps == 0:  # print every X mini-batches
+                        print(
+                            "[%d, %5d] loss: %.3f"
+                            % (epoch + 1, i + 1, running_loss / show_train_steps)
+                        )
+                        running_loss = 0.0
 
-                val_metrics = report_metrics(
-                    val_metrics, metrics, all_labels, all_preds, phase="Val"
+                # report training metrics after each epoch
+                train_metrics = self.report_metrics(
+                    train_metrics, self.metrics, all_labels, all_preds, phase="Train"
                 )
-                validation_loss /= len(val_loader)
-                print("Val loss: {0:.4f}".format(validation_loss))
-                val_total_loss.append(validation_loss)
-        if callbacks is not None:
-            for callback in callbacks:
-                callback(epoch, net, val_metrics)
 
-    total_time = (time.time() - start_time) / 60
-    print("Time trained: {:.2f} minutes".format(total_time))
+                epoch_loss /= len(train_loader)
 
-    # execute final methods of callbacks
-    if callbacks is not None:
-        for callback in callbacks:
-            # find all methods of the callback
-            method_list = [
-                func
-                for func in dir(callback)
-                if callable(getattr(callback, func)) and not func.startswith("__")
-            ]
-            if "final" in method_list:
-                # in case of model checkpoint load best model
-                if isinstance(callback, ModelCheckpoint):
-                    best_acc, best_model = callback.final()
-                    net.load_state_dict(best_model)
+                # add loss to metrics data
+                if "loss" in train_metrics:
+                    train_metrics["loss"].append(epoch_loss)
                 else:
-                    callback.final()
-    return net, best_acc, [total_loss, val_total_loss, train_metrics, val_metrics]
+                    train_metrics["loss"] = [epoch_loss]
+
+            # validate every x iterations
+            if epoch % show_validation_epochs == 0:
+                self.model.eval()
+                validation_loss = 0.0
+                all_preds = []
+                all_labels = []
+
+                with torch.no_grad():
+                    for i, data in enumerate(val_loader):
+                        inputs, labels = data["image"], data["label"]
+                        # wrap data in Variable
+                        inputs, labels = (
+                            Variable(inputs.cuda(gpu)),
+                            Variable(labels.cuda(gpu)),
+                        )
+                        # forward pass only
+                        outputs = self.model(inputs)
+                        # compute validation accuracy
+                        sigmoid = F.sigmoid(outputs)
+                        predicted = sigmoid.data >= self.class_threshold
+                        loss = self.criterion(outputs, labels)
+                        for j in range(len(predicted)):
+                            all_preds.append(predicted[j].cpu().numpy()[0])
+                            all_labels.append(labels[j].cpu().numpy()[0])
+
+                        validation_loss += loss.item()
+
+                    val_metrics = self.report_metrics(
+                        val_metrics, self.metrics, all_labels, all_preds, phase="Val"
+                    )
+                    validation_loss /= len(val_loader)
+                    print("Val loss: {0:.4f}".format(validation_loss))
+                    # add loss to metrics data
+                    if "loss" in val_metrics:
+                        val_metrics["loss"].append(validation_loss)
+                    else:
+                        val_metrics["loss"] = [validation_loss]
+
+            if self.callbacks is not None:
+                for callback in self.callbacks:
+                    callback(self, epoch, val_metrics)
+        return self.model, self.finish_training([train_metrics, val_metrics])
+
+    def finish_training(self, report):
+        total_time = (time.time() - self.start_time) / 60
+        print("Time trained: {:.2f} minutes".format(total_time))
+        # execute final methods of callbacks
+        if self.callbacks is not None:
+            for callback in self.callbacks:
+                # find all methods of the callback
+                method_list = [
+                    func
+                    for func in dir(callback)
+                    if callable(getattr(callback, func)) and not func.startswith("__")
+                ]
+                if "final" in method_list:
+                    # in case of model checkpoint load best model
+                    if isinstance(callback, ModelCheckpoint):
+                        best_acc, best_model = callback.final()
+                        self.model.load_state_dict(best_model)
+                    else:
+                        callback.final()
+        return self.model, best_acc, report
 
 
-def visualize_training(report, metrics=[]):
-    plt.figure()
-    plt.plot(report[0])
-    plt.plot(report[1])
-    plt.title("Loss during training")
-    plt.legend(["Train", "Val"])
-    plt.show()
-
-    for metric in metrics:
+    def visualize_training(self, report, metrics=None):
+        # Plot loss first
         plt.figure()
-        plt.plot(report[2][metric.__name__])
-        plt.plot(report[3][metric.__name__])
+        plt.plot(report[0]["loss"])
+        plt.plot(report[1]["loss"])
+        plt.title("Loss during training")
         plt.legend(["Train", "Val"])
-        plt.title(metric.__name__)
+        plt.show()
+        if metrics is None:
+            metrics = self.metrics
+        for metric in metrics:
+            plt.figure()
+            plt.plot(report[0][metric.__name__])
+            plt.plot(report[1][metric.__name__])
+            plt.legend(["Train", "Val"])
+            plt.title(metric.__name__)
+            plt.show()
+
+
+    def evaluate_model(self, val_loader, gpu=0):
+        # predict on the validation set
+        all_preds = []
+        all_labels = []
+        with torch.no_grad():
+            for i, data in enumerate(val_loader):
+                inputs, labels = data["image"], data["label"]
+                # wrap data in Variable
+                inputs, labels = Variable(inputs.cuda(gpu)), Variable(labels.cuda(gpu))
+                # forward + backward + optimize
+                outputs = self.model(inputs)
+                sigmoid = F.sigmoid(outputs)
+                for j, out in enumerate(sigmoid):
+                    cls = 0 if out.data[0] < 0.5 else 1
+                    all_preds.append(cls)
+                    all_labels.append(data["label"][j][0])
+
+        # compute confusion matrix
+        cm = confusion_matrix(all_labels, all_preds)
+        plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+
+        # Visualize the confusion matrix
+        classes = ["control", "patient"]
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
+
+        fmt = "d"
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(
+                j,
+                i,
+                format(cm[i, j], fmt),
+                horizontalalignment="center",
+                color="white" if cm[i, j] > thresh else "black",
+            )
+        plt.title("Confusion Matrix")
+        plt.ylabel("True label")
+        plt.xlabel("Predicted label")
         plt.show()
 
 
-def evaluate_model(net, val_loader, gpu=0):
-    # predict on the validation set
-    all_preds = []
-    all_labels = []
-    with torch.no_grad():
-        for i, data in enumerate(val_loader):
-            inputs, labels = data["image"], data["label"]
-            # wrap data in Variable
-            inputs, labels = Variable(inputs.cuda(gpu)), Variable(labels.cuda(gpu))
-            # forward + backward + optimize
-            outputs = net(inputs)
-            sigmoid = F.sigmoid(outputs)
-            for j, out in enumerate(sigmoid):
-                cls = 0 if out.data[0] < 0.5 else 1
-                all_preds.append(cls)
-                all_labels.append(data["label"][j][0])
-
-    # compute confusion matrix
-    cm = confusion_matrix(all_labels, all_preds)
-    plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
-
-    # Visualize the confusion matrix
-    classes = ["control", "patient"]
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-
-    fmt = "d"
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(
-            j,
-            i,
-            format(cm[i, j], fmt),
-            horizontalalignment="center",
-            color="white" if cm[i, j] > thresh else "black",
-        )
-    plt.title("Confusion Matrix")
-    plt.ylabel("True label")
-    plt.xlabel("Predicted label")
-    plt.show()
-
-
-def report_metrics(metrics_dict, metrics, all_labels, all_preds, phase):
-    """ Function to compute a list of metric functions. """
-    for metric in metrics:
-        result = metric(all_labels, all_preds)
-        if metric.__name__ in metrics_dict:
-            metrics_dict[metric.__name__].append(result)
-        else:
-            metrics_dict[metric.__name__] = [result]
-        # print result
-        if isinstance(result, float):
-            print("{} {}: {:.2f} %".format(phase, metric.__name__, result * 100))
-        else:
-            print("{} {}: {} ".format(phase, metric.__name__, str(result)))
-    return metrics_dict
+    def report_metrics(self, metrics_dict, metrics, all_labels, all_preds, phase):
+        """ Function to compute a list of metric functions. """
+        for metric in metrics:
+            # report everything but loss
+            if metric.__name__ is not "loss": 
+                result = metric(all_labels, all_preds)
+                if metric.__name__ in metrics_dict:
+                    metrics_dict[metric.__name__].append(result)
+                else:
+                    metrics_dict[metric.__name__] = [result]
+                # print result
+                if isinstance(result, float):
+                    print("{} {}: {:.2f} %".format(phase, metric.__name__, result * 100))
+                else:
+                    print("{} {}: {} ".format(phase, metric.__name__, str(result)))
+        return metrics_dict
