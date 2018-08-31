749afc552a6658a970cb3eadce199661ecca9511
RoshanRane <rosh1992@gmail.com>
2018-08-31 18:23:32 +0200

    minor fix

--- tests/pytests_transforms.py
+++ tests/pytests_transforms.py
@@ -1,5 +1,6 @@
+import os, sys
+sys.path.insert(0, os.path.abspath("../"))
 from nitorch.transforms import IntensityRescale
-
 if __name__ == "__main__":
-    intensity = IntensityRescale(masked=False, on_gpu=True)
+	intensity = IntensityRescale(masked=False, on_gpu=True)
 	print("hello world")
